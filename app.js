eb54d1dccb7f5c90364d4cd704aa796dd4e83a38
drs-az <204786705+drs-az@users.noreply.github.com>
2025-07-21 13:24:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ function renderPlan() {
       <button type="submit">Save Workout</button>
     </form>
     <br>
-    <button onclick="renderAddExerciseForm()">Add New Exercise</button>
+    <button onclick="renderAddExerciseForm()">Add New Exercise</button><br>
     <button onclick="renderManageExercises()">Manage Custom Exercises</button>
   `;
 
